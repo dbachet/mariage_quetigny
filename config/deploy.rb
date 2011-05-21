e6341329dbdde4f64a56a9786fe490d013257e7c
Damien Bachet <onewaytodaf@gmail.com>
2011-05-21 18:39:48 +0200

    changed to use mysql2  + capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,22 @@
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+set :repository, "git@github.com:dbachet/mariage_quetigny.git"  # Your clone URL
+set :scm, "git"
+set :user, "dagidaf"  # The server's user for deploys
+set :scm_passphrase, "jaibienmangecesoir"  # The deploy user's password
+
+ssh_options[:forward_agent] = true
+
+set :branch, "master"
+
+set :scm_verbose, true
+
+set :deploy_via, :remote_cache
+
 #http://grigio.org/deploy_rails_3_passenger_apache_dreamhost
 set :application, "mariage_quetigny"
 set :repository,  "git@github.com:dbachet/mariage_quetigny.git"
 set :domain, "changeons.org"
-set :scm, 'git'
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-set :shared_path, "/home/#{user}/.gems"
+#set :shared_path, "/home/#{user}/.gems"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :user, 'dagidaf'
@@ -14,17 +24,16 @@ set :deploy_to, "/home/#{user}/#{domain}/#{application}"
 set :deploy_via, :export
 set :rails_env, "production"
 
-default_run_options[:pty] = true
 set :use_sudo, false
 #before "deploy", "deploy:check_revision"
-set :ssh_options, {:forward_agent => true}
 
-after "deploy:update_code", "deploy:write_revision"
-before "deploy:gems", "deploy:symlink"
+#after "deploy:update_code", "deploy:write_revision"
+#before "deploy:gems", "deploy:symlink"
 
-role :web, "changeons.org"                          # Your HTTP server, Apache/etc
-role :app, "changeons.org"                          # This may be the same as your `Web` server
-role :db,  "changeons.org", :primary => true # This is where Rails migrations will run
+#role :web, "changeons.org"                          # Your HTTP server, Apache/etc
+#role :app, "changeons.org"                          # This may be the same as your `Web` server
+#role :db,  "changeons.org", :primary => true # This is where Rails migrations will run
+server "changeons.org", :app, :web, :db, :primary => true
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -45,11 +54,14 @@ role :db,  "changeons.org", :primary => true # This is where Rails migrations wi
  
 namespace :deploy do
 
-  desc "expand the gems"
-  task :gems, :roles => :web, :except => { :no_release => true } do
-    run "cd #{current_path}; #{shared_path}/bin/bundle unlock"
-    run "cd #{current_path}; nice -19 #{shared_path}/bin/bundle install vendor/" # nice -19 is very important otherwise DH will kill the process!
-    run "cd #{current_path}; #{shared_path}/bin/bundle lock"
+#  desc "expand the gems"
+#  task :gems, :roles => :web, :except => { :no_release => true } do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+#    run "cd #{current_path}; #{shared_path}/bin/bundle unlock"
+#    run "cd #{current_path}; nice -19 #{shared_path}/bin/bundle install vendor/" # nice -19 is very important otherwise DH will kill the process!
+#    run "cd #{current_path}; #{shared_path}/bin/bundle lock"
+    
   end
 
 end
